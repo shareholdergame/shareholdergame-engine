12a8471ed0130f1e74133e61eef81b9c9fbd78ea
alex <admin@stockholdergame.com>
2019-12-04 07:24:08 +0100

    Game core implementation

---
+++ shareholdergame-game/game-core/src/main/java/com/shareholdergame/engine/game/core/GameCoreFacade.java
@@ -0,0 +1,43 @@
+package com.shareholdergame.engine.game.core;
+
+public class GameCoreFacade {
+
+    public GameInitializer gameInitializer() {
+        return new GameInitializer();
+    }
+
+    public void doTurn(Turn turn) {
+
+    }
+
+    public GameReport gameReport() {
+        return null;
+    }
+
+    public static class GameInitializer {
+
+        private GameInitializer() {
+        }
+
+        public GameInitializer withId(Long gameSetId) {
+            return this;
+        }
+
+        public GameInitializer withPlayers(String... players) {
+            return this;
+        }
+
+        public GameInitializer withCardsOption(int major, int minor) {
+            return this;
+        }
+
+        public GameReport initialize() {
+            GameSet gameSet = GameSet.builder()
+                    .share(1L, 100)
+                    .build();
+
+
+            return null;
+        }
+    }
+}
--- shareholdergame-game/game-core/src/main/java/com/shareholdergame/engine/game/core/GameSet.java
+++ shareholdergame-game/game-core/src/main/java/com/shareholdergame/engine/game/core/GameSet.java
@@ -19,7 +19,26 @@ public class GameSet {
     private GameSet(GameSetBuilder builder) {
     }
 
-    private static class GameSetBuilder implements Builder<GameSet> {
+    public static GameSetBuilder builder() {
+        return new GameSetBuilder();
+    }
+
+    public static class GameSetBuilder implements Builder<GameSet> {
+
+        private GameSetBuilder() {
+        }
+
+        public GameSetBuilder cardOption(int major, int minor) {
+            return this;
+        }
+
+        public GameSetBuilder priceScale(int min, int max, int step) {
+            return this;
+        }
+
+        public GameSetBuilder share(Long shareId, int initialPrice) {
+            return this;
+        }
 
         @Override
         public GameSet build() {
